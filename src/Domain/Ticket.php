955b01ed7255876439fdec3be32c8970bc31d193
santik <santik.alex@gmail.com>
2017-09-10 14:31:55 +0200

    Ticket object and test

---
+++ src/Domain/Ticket.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Santik\Tickets\Domain;
+
+use JsonSerializable;
+use Webmozart\Assert\Assert;
+
+final class Ticket implements JsonSerializable
+{
+    private $barcodes;
+
+    private $id;
+
+    public static function createFromArray(array $data): Ticket
+    {
+        self::guardData($data);
+
+        $id = isset($data['id']) ? $data['id'] : null;
+
+        return new Ticket($data['barcodes'], $id);
+    }
+
+    private function __construct(array $barcodes, $id = null)
+    {
+
+        $this->barcodes = $barcodes;
+        $this->id = $id;
+    }
+
+    private static function guardData(array $data)
+    {
+        Assert::keyExists($data, 'barcodes');
+        Assert::isArray($data['barcodes']);
+        Assert::notEmpty($data['barcodes']);
+
+        foreach ($data['barcodes'] as $barcode) {
+            Assert::string($barcode);
+            Assert::notEmpty($barcode);
+        }
+    }
+
+    public function barcodes(): array
+    {
+        return $this->barcodes;
+    }
+
+    public function id()
+    {
+        return $this->id;
+    }
+
+    public function toArray()
+    {
+        return [
+            'id' => $this->id(),
+            'barcodes' => $this->barcodes()
+        ];
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+}
---
+++ tests/Domain/TicketTest.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Santik\Tickets\Domain;
+
+use PHPUnit\Framework\TestCase;
+
+class TicketTest extends TestCase
+{
+    public function testCreateFromArray_WithCorrectParams_WillReturnTicket()
+    {
+        $data = [
+            'barcodes' => [
+                'foo',
+                'bar'
+            ],
+        ];
+
+        $ticket = Ticket::createFromArray($data);
+
+        $this->assertInstanceOf(Ticket::class, $ticket);
+        $this->assertEquals($ticket->barcodes(), $data['barcodes']);
+        $this->assertEquals($ticket->id(), null);
+    }
+
+    public function testCreateFromArray_WithCorrectParamsAndId_WillReturnTicket()
+    {
+        $data = [
+            'barcodes' => [
+                'foo',
+                'bar'
+            ],
+            'id' => 123
+        ];
+
+        $ticket = Ticket::createFromArray($data);
+
+        $this->assertEquals($ticket->id(), $data['id']);
+    }
+
+    public function testCreateFromArray_WithNoBarcodes_WillThrowException()
+    {
+        $data = [
+            'some'
+        ];
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        Ticket::createFromArray($data);
+    }
+
+    public function testCreateFromArray_WithEmptyBarcodes_WillThrowException()
+    {
+        $data = [
+            'barcodes' => []
+        ];
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        Ticket::createFromArray($data);
+    }
+
+    public function testCreateFromArray_WithEmptyBarcode_WillThrowException()
+    {
+        $data = [
+            'barcodes' => [
+                ''
+            ]
+        ];
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        Ticket::createFromArray($data);
+    }
+
+    public function testToArray_WillReturnCorrectData()
+    {
+        $data = [
+            'barcodes' => [
+                'foo',
+                'bar'
+            ],
+            'id' => 123
+        ];
+
+
+
+        $ticket = Ticket::createFromArray($data);
+        $this->assertEquals($data, $ticket->toArray());
+    }
+
+    public function testIsJsonSerializable()
+    {
+        $data = [
+            'barcodes' => [
+                'foo',
+                'bar'
+            ],
+            'id' => 123
+        ];
+
+
+
+        $ticket = Ticket::createFromArray($data);
+        $this->assertInstanceOf(\JsonSerializable::class, $ticket);
+    }
+}
