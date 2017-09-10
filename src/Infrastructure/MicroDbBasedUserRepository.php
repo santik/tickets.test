4494e3069b4224f36ab6a70e466782e75e725a8c
santik <santik.alex@gmail.com>
2017-09-10 14:37:34 +0200

    user repo implementation

---
+++ src/Infrastructure/MicroDbBasedUserRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Santik\Tickets\Infrastructure;
+
+use MicroDB\Database;
+use Santik\Tickets\Domain\Repository\UserRepository;
+use Santik\Tickets\Domain\User;
+
+class MicroDbBasedUserRepository implements UserRepository
+{
+    private $database;
+
+    public function __construct(Database $database)
+    {
+        $this->database = $database;
+    }
+
+    public function createUser($username): User
+    {
+        $id = $this->database->create([
+            'type' => 'user',
+            'name' => $username
+        ]);
+
+        $userData = [
+            'id' => $id,
+            'name' => $username
+        ];
+
+        return User::createFromArray($userData);
+    }
+
+    public function findUser(int $userId): User
+    {
+        $user = $this->database->load($userId);
+        if ($user['type'] != 'user') {
+            throw new \Exception('No user found');
+        }
+
+        $data = [
+            'id' => $userId,
+            'name' => $user['name']
+        ];
+
+
+        return User::createFromArray($data);
+    }
+}
